fef2a6ddf6ac4bcc81096eb61103d2e9e18a1af2
azreallpw <78826249+azreallpw@users.noreply.github.com>
2021-05-04 00:04:04 -0500

    Add files via upload

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,23 @@
+//D3 Library as JSON
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0]; 
+        var PANEL = d3.select("#sample-metadata");
+        PANEL.html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key.toUppercase()}: ${value}`);
+        });
+})};
+
+function buildChart(sample) {
+    d3.json("samples.json").then((data) => {
+        var sample = data.samples;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0]; 
+        var otu_ids = result.otu_ids; 
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+})};
